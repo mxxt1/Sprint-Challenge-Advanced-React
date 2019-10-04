89804e0bfab3c974312a7f55220fc34f87989ef1
mxxt1 <matt@dustcloudlabs.com>
2019-10-04 11:55:02 -0400

    added axios calls, getting data, correctly setting to state

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import axios from 'axios';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      data: []
+    }
+  }
+
+  componentDidMount(){
+    axios.get('http://localhost:5000/api/players')
+    .then(response => {
+      console.log(response);
+      this.setState({data: response.data})
+      //is there a 100 element length limit on arrays?
+      console.log(this.state.data)
+    })
+    .catch(error => {
+      console.log(`CDU error: `, error);
+    })
+  }
+  
+  
+  
+  render() {
+    return (
+      <div className='App'>
+        <h1>Soccer Player Tracker</h1>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
+
